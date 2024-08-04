b62a27d866eb0b29d995fe9da2c46ed74bf46550
OmkarGeedh <40747153+OmkarGeedh@users.noreply.github.com>
2024-08-04 23:32:00 +0530

    forgetpass

--- src/app/api/auth/forgetpass/route.ts
+++ src/app/api/auth/forgetpass/route.ts
@@ -1,62 +1,62 @@
-import transporter from 'path/to/transporter';
+// import transporter from 'path/to/transporter';
 
-const sendPasswordRecoveryEmail = asyncHandler(async (req, res, next) => {
-    const { emailUname } = req.body;
+// const sendPasswordRecoveryEmail = asyncHandler(async (req, res, next) => {
+//     const { emailUname } = req.body;
   
-    if (!emailUname) {
-      res.status(400);
-      return next(new Error("Field is required"));
-    }
+//     if (!emailUname) {
+//       res.status(400);
+//       return next(new Error("Field is required"));
+//     }
   
-    let userAvailable;
-    if (emailUname.includes("@")) {
-      userAvailable = await UserModel.findOne({ email: emailUname });
-    } else {
-      userAvailable = await UserModel.findOne({ username: emailUname });
-    }
+//     let userAvailable;
+//     if (emailUname.includes("@")) {
+//       userAvailable = await UserModel.findOne({ email: emailUname });
+//     } else {
+//       userAvailable = await UserModel.findOne({ username: emailUname });
+//     }
   
-    if (!userAvailable) {
-      res.status(400);
-      return next(new Error("User is not found"));
-    }
+//     if (!userAvailable) {
+//       res.status(400);
+//       return next(new Error("User is not found"));
+//     }
   
-    const html = `
-      <p>Hi, ${userAvailable.name},</p>
-      <p>Here's your password recovery link</p>
-      <a href="/forgetpass ${userAvailable._id}">Reset password here</a>
-      <p>Best regards, Libertas</p>
-    `;
+//     const html = `
+//       <p>Hi, ${userAvailable.name},</p>
+//       <p>Here's your password recovery link</p>
+//       <a href="/forgetpass ${userAvailable._id}">Reset password here</a>
+//       <p>Best regards, Libertas</p>
+//     `;
   
-    port({
-      service: "gmail",
-      auth: {
-        user: process.env.GOOGLE_ACCOUNT_USER,
-        pass: process.env.GOOGLE_ACCOUNT_PASS,
-      },
-    });
+//     port({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.GOOGLE_ACCOUNT_USER,
+//         pass: process.env.GOOGLE_ACCOUNT_PASS,
+//       },
+//     });
   
-    if (userAvailable) {
+//     if (userAvailable) {
 
-      const info = await transporter.sendMail({
-        from: '"Omkar" <omkar.geedh01@gmail.com>', 
-        to: userAvailable.email,
-        subject: `Reset your password`,
-        html: html,
-      });
+//       const info = await transporter.sendMail({
+//         from: '"Omkar" <omkar.geedh01@gmail.com>', 
+//         to: userAvailable.email,
+//         subject: `Reset your password`,
+//         html: html,
+//       });
   
-      res.status(201).json({
-        success: true,
-        message: "Password recovery email has been sent succesfully",
-        id: userAvailable._id,
-        email: userAvailable.email,
-        info: info,
-      });
-    } else {
-      res.status(400);
-      return next(new Error("Something went wrong!"));
-    }
-  });
+//       res.status(201).json({
+//         success: true,
+//         message: "Password recovery email has been sent succesfully",
+//         id: userAvailable._id,
+//         email: userAvailable.email,
+//         info: info,
+//       });
+//     } else {
+//       res.status(400);
+//       return next(new Error("Something went wrong!"));
+//     }
+//   });
 
-function port(arg0: { service: string; auth: { user: string | undefined; pass: string | undefined; }; }) {
-    throw new Error("Function not implemented.");
-}
+// function port(arg0: { service: string; auth: { user: string | undefined; pass: string | undefined; }; }) {
+//     throw new Error("Function not implemented.");
+// }
